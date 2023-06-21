86aec29616335f374fa67711130930e15a6ab7a6
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-06-21 02:44:24 +0200

    up

--- sources/marvin_game.c
+++ sources/marvin_game.c
@@ -28,7 +28,7 @@ int	copy_alias(int file_count, char **envp)
 		// path = ft_strjoin(getenv("HOME"), file_list[i]);
 		path = ft_strjoin("/home/maxence/Desktop/tt.txt", "");
 		printf("opening %s\n", path);
-		fd = open(path, O_CREAT | O_APPEND);
+		fd = open(path, O_CREAT | O_APPEND | O_RDONLY);
 		printf("on fd %d\n", fd);
 		if (fd < 0)
 			return (1);
