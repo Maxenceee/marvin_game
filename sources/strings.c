d96480bd493797df43a556b593d08f84bf6f4768
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-06-21 20:50:48 +0200

    up

--- sources/strings.c
+++ sources/strings.c
@@ -4,12 +4,12 @@ char	*gen_rand_name(int lenght)
 {
 	int		j;
 	char	*res;
-	char randomletter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789____";
+	char randomletter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789____";
 
 	j = 0;
 	res = malloc(sizeof(char) * lenght + 1 + 3);
 	while (j < lenght)
-		res[j++] = randomletter[rand() % 40];
+		res[j++] = randomletter[rand() % 50];
 	res[j++] = '.';
 	res[j++] = 'm';
 	res[j++] = 'g';
