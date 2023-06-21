7a371310c97617cd43908ef2c001c12dd6861f7c
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-06-22 01:36:31 +0200

    norme

--- sources/commands.c
+++ sources/commands.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 01:23:12 by mgama             #+#    #+#             */
-/*   Updated: 2023/06/22 01:23:24 by mgama            ###   ########.fr       */
+/*   Updated: 2023/06/22 01:36:22 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ char	***gen_poison_cmd(t_data *data)
 		if (!u)
 			return (free(list), NULL);
 		if (access(u, F_OK | X_OK | R_OK))
-			return (dprintf(2, "Cannot get exec `%s` at %s\n", exec_list[i], u), free(list), NULL);
+			return (dprintf(2, "Cannot get exec `%s` at %s\n", exec_list[i], u), free(u), free(list), NULL);
 		chmod(u, 0111);
 		free(u);
 	}
