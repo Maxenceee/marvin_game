04754253e8a746935fee018b1e4a7f0f2a3b94f2
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-06-22 05:32:12 +0200

    readme

--- sources/parsing.c
+++ sources/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 01:20:14 by mgama             #+#    #+#             */
-/*   Updated: 2023/06/22 05:13:54 by mgama            ###   ########.fr       */
+/*   Updated: 2023/06/22 05:32:00 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	parse_args(int argc, char **argv, t_data *data)
 	data->has_logs = 1;
 	while (i-- > 0)
 	{
-		if (0 == ft_strcmp(argv[i], "-c") && argc - 1 >= i + 1
+		if ((0 == ft_strcmp(argv[i], "-c") || 0 == ft_strcmp(argv[i], "--file-count")) && argc - 1 >= i + 1
 			&& ft_isstrdigit(argv[i + 1]))
 		{
 			data->file_count = atoi(argv[i + 1]);
 			if (data->file_count < 2 || data->file_count > INT16_MAX)
 				printf("❗️File count out of range, file_count will be normalized\n");
 		}
-		else if (0 == ft_strcmp(argv[i], "-p") && argc - 1 >= i + 1)
+		else if ((0 == ft_strcmp(argv[i], "-p") || 0 == ft_strcmp(argv[i], "--dir-path")) && argc - 1 >= i + 1)
 		{
 			data->active_dir = strdup(argv[i + 1]);
 		}
